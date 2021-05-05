e75028cb39d0e4cc8f26c1537efff3fb6fc486e8
Rhine Wang <rhine1217@gmail.com>
2021-05-04 22:47:20 -0700

    permissions for update controller

--- boss-guide-api/main_app/permissions.py
+++ boss-guide-api/main_app/permissions.py
@@ -1,8 +1,9 @@
 from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
-  def has_object_permission(self, request, view, obj):
-    print('got here')
-    if request.method in permissions.SAFE_METHODS:
+  def has_permission(self, request, view):
       return True
-    return obj['created_by'] == request.usre
+  def has_object_permission(self, request, view, obj):
+      if request.method in permissions.SAFE_METHODS:
+        return True
+      return obj.created_by == request.user
--- boss-guide-api/main_app/views/setup.py
+++ boss-guide-api/main_app/views/setup.py
@@ -2,6 +2,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 from main_app.permissions import IsOwner
 from main_app.models import BossSetup, PlayerSetup
 from main_app.serializers import BossSetupListSerializer, BossSetupCreateUpdateSerializer, PlayerSetupCreateUpdateSerializer
@@ -65,33 +66,40 @@ class BossPlayerSetupCreate(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class BossPlayerSetupUpdate(APIView):
+    permission_classes = (IsOwner,)
+    
+    def get_object(self):
+        slug = self.kwargs['slug']
+        boss_setup_id = hashids.decode(slug)[0]
+        obj = get_object_or_404(BossSetup.objects.all(), pk=boss_setup_id)
+        print('inside get_object')
+        print('checking perm', self.check_object_permissions(self.request, obj))
+        self.check_object_permissions(self.request, obj)
+        return obj
+      
     def patch(self, request, slug, format=None):
         print('fdasfsdsas')
-        boss_setup_id = hashids.decode(slug)[0]
-        boss_setup_obj = BossSetup.objects.get(id=boss_setup_id)
-        print(request.user)
-        print(boss_setup_obj.created_by)
-        if request.user == boss_setup_obj.created_by:
-            print('inside here')
-            boss_setup_data = request.data['bossSetup']
-            player_setups_data = request.data['playerSetups']
-            new_boss_setup = BossSetupCreateUpdateSerializer(boss_setup_obj, data=boss_setup_data, partial=True)
-            if new_boss_setup.is_valid():
-                new_boss_setup.save()
-                if player_setups_data:
-                    current_player_setups = PlayerSetup.objects.filter(boss_setup=boss_setup_id)
-                    for i, player_setup_obj in enumerate(current_player_setups):
-                        player_setup_data = {}
-                        player_setup_data['player_class'] = player_setups_data[i]['player_class']
-                        for j, skill in enumerate(player_setups_data[i]['skills']):
-                            player_setup_data[f'skill{j+1}'] = skill
-                        for k, toy in enumerate(player_setups_data[i]['toys']):
-                            player_setup_data[f'toy{k+1}'] = toy
-                        new_player_setup = PlayerSetupCreateUpdateSerializer(player_setup_obj, data=player_setup_data, partial=True)
-                        if new_player_setup.is_valid():
-                            new_player_setup.save()
-                serializer = BossSetupListSerializer(BossSetup.objects.get(pk=boss_setup_id))
-                return Response(serializer.data, status=status.HTTP_200_OK, content_type='application/json')
-        else: 
-            return Response(status=status.HTTP_403_FORBIDDEN)
+        print('isowner', IsOwner)
+        boss_setup_obj = self.get_object()
+        boss_setup_data = request.data['bossSetup']
+        player_setups_data = request.data['playerSetups']
+        new_boss_setup = BossSetupCreateUpdateSerializer(boss_setup_obj, data=boss_setup_data, partial=True)
+        if new_boss_setup.is_valid():
+            new_boss_setup.save()
+            if player_setups_data:
+                current_player_setups = PlayerSetup.objects.filter(boss_setup=boss_setup_obj.id)
+                for i, player_setup_obj in enumerate(current_player_setups):
+                    player_setup_data = {}
+                    player_setup_data['player_class'] = player_setups_data[i]['player_class']
+                    for j, skill in enumerate(player_setups_data[i]['skills']):
+                        player_setup_data[f'skill{j+1}'] = skill
+                    for k, toy in enumerate(player_setups_data[i]['toys']):
+                        player_setup_data[f'toy{k+1}'] = toy
+                    new_player_setup = PlayerSetupCreateUpdateSerializer(player_setup_obj, data=player_setup_data, partial=True)
+                    if new_player_setup.is_valid():
+                        new_player_setup.save()
+            serializer = BossSetupListSerializer(BossSetup.objects.get(pk=boss_setup_obj.id))
+            return Response(serializer.data, status=status.HTTP_200_OK, content_type='application/json')
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class BossSetupDestroy(generics.DestroyAPIView):
